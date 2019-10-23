8e206161e8f0b8af8f1302c61e4f4183950513a3
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2019-10-23 17:09:08 +0100

    world 5 starting

--- src/game/world5/level2.lean
+++ src/game/world5/level2.lean
@@ -15,14 +15,17 @@ rw le_def,
 
 and now you can use `cases` on `h` and `use` for the goal.
 
-Pro tip: the rewrites aren't actually necessary!
+Pro tip: `rw le_def at h ⊢` does both rewrites at once.
+You can get the goal sign by typing `\|-`.
+
+Even pro-er tip: the rewrites aren't actually necessary!
 They are true by definition, so actually you can just skip them.
 It's more readable if you put them in, but quicker if you don't.
 -/
 
 theorem le_succ {a b : mynat} (h : a ≤ b) : a ≤ (succ b) :=
 begin [less_leaky]
-  rw le_def at h,
+  rw le_def at h ⊢,
   cases h with c hc,
   use (succ c),
   rw hc,
