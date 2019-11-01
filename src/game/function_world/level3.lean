6fcc8d17f4a197e0e3abb5968c05df0294f1dacf
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2019-11-01 09:58:36 +0000

    functions

--- src/game/function_world/level3.lean
+++ src/game/function_world/level3.lean
@@ -1,6 +1,11 @@
 /- Tactic : intro
 If your goal is a function `⊢ P → Q` then `intro` is often the
-tactic you will use to proceed. `intro p,` is Lean's way
+tactic you will use to proceed. What does it mean to define
+a function? Given an arbitrary term of type `P` (or an element
+of the set `P` if you think set-theoretically) you need
+to come up with a term of type `Q`, so your first step is
+to choose `p`, an arbitary 
+`intro p,` is Lean's way
 of saying "let $p ∈ P$ be arbitrary". `intro p` changes
 
 ```
