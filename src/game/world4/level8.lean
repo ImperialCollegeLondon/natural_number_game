b22a97f46d008e5c412f3dd3abd8a0d931e1626d
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2019-11-01 22:40:54 +0000

    v1.04 final

--- src/game/world4/level8.lean
+++ src/game/world4/level8.lean
@@ -1,5 +1,4 @@
 import game.world4.level7 -- hide
-import tactic.ring
 namespace mynat -- hide
 
 /- 
@@ -8,40 +7,39 @@ namespace mynat -- hide
 
 ## Level 8 : `add_squared`
 
-[final boss music]
+[final boss music] 
+
+You see something written on the stone dungeon wall:
+```
+begin
+  rw one_eq_succ_zero,
+  repeat {rw pow_succ},
+  ...
+```
+
+and you can't make out the rest.
+
+[editor's note: Actual Lean natural
+numbers do have `2`, but I figured now was no time to
+introduce it and anyway the first thing you'd do with
+it would be change it to `succ(1)` anyway]
+
 -/
 
 /- Theorem
 For all naturals $a$ and $b$, we have
-$$(a+b)^2=a^2+b^2+2*a*b.$$
+$$(a+b)^2=a^2+b^2+2ab.$$
 -/
 lemma add_squared (a b : mynat) :
   (a + b) ^ (succ(1)) = 
 a ^ (succ(1)) + b^(succ(1)) + (succ(1))*a*b :=
 begin [less_leaky]
-  rw pow_succ,
-  rw pow_succ,
-  rw pow_succ,
-  rw one_eq_succ_zero,
-  rw pow_succ,
-  rw pow_succ,
-  rw pow_succ,
-  rw pow_zero,
-  rw pow_zero,
-  rw pow_zero,
-  rw one_mul,
-  rw one_mul,
-  rw one_mul,
-  rw mul_add,
-  rw add_mul,
-  rw add_mul,
-  rw succ_mul,
-  rw succ_mul,
-  rw zero_mul,
-  rw zero_add,
-  rw add_mul,
-  rw mul_comm b,
-  rw add_assoc,
+  rw [pow_succ, pow_succ, pow_succ, one_eq_succ_zero],
+  rw [pow_succ, pow_succ, pow_succ, pow_zero, pow_zero],
+  rw [pow_zero, one_mul, one_mul, one_mul, mul_add],
+  rw [add_mul, add_mul, succ_mul, succ_mul],
+  rw [zero_mul, zero_add, add_mul, mul_comm b],
+  rw add_assoc, 
   rw ←add_assoc (a * b),
   rw add_comm _ (b * b),
   rw ←add_assoc,
