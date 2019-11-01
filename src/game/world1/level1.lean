5a4d93908a8ec65393c887d7494bbb29dc47200f
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2019-11-01 09:29:58 +0000

    v1.03

--- src/game/world1/level1.lean
+++ src/game/world1/level1.lean
@@ -6,7 +6,7 @@ namespace mynat -- hide
 -- World name : Tutorial world
 
 /- 
-# The Natural Number Game, version 1.02.
+# The Natural Number Game, version 1.03.
 
 ## By Kevin Buzzard and Mohammad Pedramfar. 
 
