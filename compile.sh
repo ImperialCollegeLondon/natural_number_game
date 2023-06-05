54f510a99612d65d4df729860dfa31dfa801ab52
MithicSpirit <rpc01234@gmail.com>
2023-06-05 15:46:23 -0400

    Fix compile.sh run_locally.sh
    - More readable (multiline)
    - Use python http.server for running locally since that should be on
      everyone's systems by default
    - Use globs for deleting olean artifacts since rm_olean doesn't seem to
      exist

--- compile.sh
+++ compile.sh
@@ -1,1 +1,5 @@
-rm -rf html/ ; cd src/game/ ; rm_olean ; cd ../../ ; make-lean-game ; cp src/game/LaTeX/implies_diag.jpg src/game/LaTeX/function_diag.jpg src/game/LaTeX/FAQ.html html/
+rm -rf html/
+rm -f src/game/**/*.olean
+make-lean-game
+
+cp src/game/LaTeX/implies_diag.jpg src/game/LaTeX/function_diag.jpg src/game/LaTeX/FAQ.html html/
